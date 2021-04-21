439e6c97e96ebe0933b313b65365b040bd4db4bf
brumatt <robsonbrumatt@gmail.com>
2021-04-21 13:25:07 -0300

    Adicionado Index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <title>Sistema Beta Adenilson Reis</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Interfaces/Principal/SistemaB.css">
+    <script type="text/javascript" src="Interfaces/Principal/SistemaB.js"></script>
+</head>
+<header id="cabecario" class="FotosReducao">
+
+
+</header>
+<body>
+<div id="divCorpo">
+    Div Principal
+    
+    
+    
+    
+    
+    
+    </div>
+</body>
+<footer id="rodape">
+rodape
+
+</footer>
+</html>
