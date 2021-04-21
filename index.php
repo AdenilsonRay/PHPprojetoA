d12d91754b83bfb002e2d3f09ca4ac64c3df7ffe
brumatt <robsonbrumatt@gmail.com>
2021-04-21 14:16:32 -0300

    Criando O menu superior A

--- index.php
+++ index.php
@@ -6,12 +6,37 @@
     <link rel="stylesheet" href="Interfaces/Principal/SistemaB.css">
     <script type="text/javascript" src="Interfaces/Principal/SistemaB.js"></script>
 </head>
-<header id="cabecario" class="FotosReducao">
+<header id="cabecarioI" class="FotosReducao">
+    
+    <div id="divCabecarioI" name="divCabecarioN">
+        <div id="divLogoI" name="divLogoN">
+            <img id="imgLogoI" name="imgLogoN" href="Imagens/Logotipo Arauto Pequena.jpg" alt="Logotipo">
+        </div>
+        
+        <nav id="navLogoI" name="navLogoN">
+            <ul class="menu">
+                <li><a href="#">Home</a>Home</li>
+                <li><a href="#">Sobre</a>Sobre</li>
+                <li><a href="#">O que Fazemos?</a>
+                    <ul>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">SEO</a></li>
+                        <li><a href="#">Design</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Links</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
 
+        <div id="divAcessoI" name="divAcessoN">
+            <label id="labAcessoI" name="divAcessoN">ACESSO:</label>
+        </div>
+    </div>
 
 </header>
 <body>
-<div id="divCorpo">
+<div id="divCorpoI" name="divCorpoN">
     Div Principal
     
     
@@ -21,7 +46,7 @@
     
     </div>
 </body>
-<footer id="rodape">
+<footer id="rodapeI" name="rodapeN">
 rodape
 
 </footer>
