45cc840f469dd725d3da8a1533f0ada94c474727
brumatt <robsonbrumatt@gmail.com>
2021-04-23 10:37:19 -0300

    Criando o login

--- index.php
+++ index.php
@@ -3,40 +3,62 @@
 <head>
     <title>Sistema Beta Adenilson Reis</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="Interfaces/Principal/SistemaB.css">
+    <link rel="stylesheet" type="text/css" href="Interfaces/Principal/SistemaB.css">
     <script type="text/javascript" src="Interfaces/Principal/SistemaB.js"></script>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 </head>
 <header id="cabecarioI" class="FotosReducao">
     
     <div id="divCabecarioI" name="divCabecarioN">
-        <div id="divLogoI" name="divLogoN">
-            <img id="imgLogoI" name="imgLogoN" href="Imagens/Logotipo Arauto Pequena.jpg" alt="Logotipo">
+        
+        <div id="divLogoI" name="divLogoN" class="divCabec">
+            <img id="imgLogoI" name="imgLogoN" src="Imagens/Logotipo Arauto A PEQ2.png" alt="Logotipo" width="150px" height="120px">
         </div>
         
-        <nav id="navLogoI" name="navLogoN">
-            <ul class="menu">
-                <li><a href="#">Home</a>Home</li>
-                <li><a href="#">Sobre</a>Sobre</li>
-                <li><a href="#">O que Fazemos?</a>
-                    <ul>
-                        <li><a href="#">Web Design</a></li>
-                        <li><a href="#">SEO</a></li>
-                        <li><a href="#">Design</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Links</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
+        <div id="divMenuI" name="divMenuN" class="divCabec">
+            <nav id="navLogoI" name="navLogoN">
 
-        <div id="divAcessoI" name="divAcessoN">
-            <label id="labAcessoI" name="divAcessoN">ACESSO:</label>
+                <ul class="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                        <li><a href="#">O que Fazemos?</a>
+                            <ul>
+                                <li><a href="#">Web Design</a></li>
+                                <li><a href="#">SEO</a></li>
+                                <li><a href="#">Design</a></li>
+                            </ul>
+                        </li>
+                    <li><a href="#">Links</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+
+            </nav>
+        </div>    
+
+        <div id="divAcessoI" name="divAcessoN" class="divCabec" >
+            <div class="row">
+                <div class="col-25">
+                    <label id="labNomeI" name="labNomeN">Login:</label>
+                </div>
+                <div class="col-75">
+                    <input id="inpNomeI" name="inpNomeN" type="text" class="inpClassLogin" maxlength="20">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label id="labSenhaI" name="labSenhaI">Senha:</label>
+                </div>
+                <div class="col-75">
+                    <input id="inpSenhaI" name="inpSenhaN" type="text" class="inpClassLogin" maxlength="20">
+                </div>    
+            </div> 
         </div>
+        
     </div>
 
 </header>
 <body>
-<div id="divCorpoI" name="divCorpoN">
+    <div id="divCorpoI" name="divCorpoN">
     Div Principal
     
     
